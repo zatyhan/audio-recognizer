a77ebd3c788da6dea50b25c29f3f0251479f72ad
zatyhan <nureizzaty@gmail.com>
2024-10-24 16:50:02 +0100

    added helper code for spotify configurations

---
+++ helper.py
@@ -0,0 +1,15 @@
+import spotipy
+from spotipy.oauth2 import SpotifyPKCE
+from dotenv import load_dotenv
+import os 
+
+# load_dotenv("C:/Users/Investment Officer/Downloads/eizzaty/audio-recognizer/.env")
+SPOTIFY_CLIENT_ID="d3eb6ef85c20439d89f4c6b100024b20"
+scope='playlist-modify-public'
+# print(os.getenv("SPOTIPY_CLIENT_ID"))
+auth_manager = SpotifyPKCE(client_id=SPOTIFY_CLIENT_ID, redirect_uri="https://youtify.streamlit.app/", scope=scope)
+
+sp= spotipy.Spotify(auth_manager=auth_manager)
+print(sp.search("spotify:search:Walk%20NCT%20127"))
+
+
