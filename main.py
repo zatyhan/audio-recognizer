c5f54cf04680376e91fe7caee0f3dba685affb91
zatyhan <nureizzaty@gmail.com>
2024-10-29 23:39:12 +0000

    "Added playlist name input field and assigned keys to input fields"

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from PlaylistMaker import PlaylistMaker
-from Processor import Processor
+from helpers import Processor, PlaylistMaker
 
 yt_url = input('Enter youtube URL: ')
 playlist_name = input('Enter playlist name: ')
@@ -28,16 +27,26 @@ video_length= track.video_length()
 # playlist.add_to_playlist(track_id)
 # start_time+=duration +1
 # print(start_time)
+track_ids= set()
 while start_time< video_length:
     try:
-        track_title, artist=  track.recognize_audio(start_time=start_time)
-        print("track found: ", track_title)
-        track_id, duration= playlist.lookup(track_name=track_title, artist_name=artist)
-        
-        playlist.add_to_playlist(track_id)
-        start_time+=duration +1
+        track_title, artist, album_name=  track.recognize_audio(start_time=start_time)
+        print("Looking for: ", track_title)
+        track_id, found_title, duration= playlist.lookup(track_name=track_title, artist_name=artist)
+        print("Track found: ", found_title)
+        print('')
+        track_ids.add(track_id)
+        print(track_ids)
+        # playlist.add_to_playlist(track_id)
+        start_time+=duration +5
 
     except:
         print('Something went wrong')
+
+    finally:
+        for t in track_ids:
+            playlist.add_to_playlist(t)
+        
+        print('Playlist created successfully!') 
+        
     
-print('Playlist created successfully!')
